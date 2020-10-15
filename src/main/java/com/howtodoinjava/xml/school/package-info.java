55af8797bba0aed57adb45b2606c9994e3212e6f
Rslullitel <rslullitel@prokarma.com>
2020-10-15 13:20:35 -0300

    addSoapUI

--- src/main/java/com/howtodoinjava/xml/school/package-info.java
+++ src/main/java/com/howtodoinjava/xml/school/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2020.10.15 a las 11:57:49 AM ART 
+// Generado el: 2020.10.15 a las 12:48:49 PM ART 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.howtodoinjava.com/xml/school", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
